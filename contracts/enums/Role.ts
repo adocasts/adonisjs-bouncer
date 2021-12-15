19f187f55dadf47f8bb97c7840100fef4049c069
Tom Gobich <tom@tomgobich.com>
2021-12-15 18:19:05 -0500

    adding role abilities as comments and also adding editor role

--- contracts/enums/Role.ts
+++ contracts/enums/Role.ts
@@ -1,7 +1,16 @@
 enum Role {
+  // Can create comments and delete comments they created
   USER = 1,
+
+  // Can create comments and delete any comment
   MODERATOR = 2,
-  ADMIN = 3
+
+  // Can create comments and delete comments they created
+  // Can create posts and edit and delete posts they created
+  EDITOR = 3,
+
+  // Can do everything
+  ADMIN = 4
 }
 
 export default Role
